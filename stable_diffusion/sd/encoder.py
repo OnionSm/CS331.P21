1daff909e1393e745268ac3ab990763327b86496
OnionSm <vuongha16104@gmail.com>
2025-05-20 22:11:15 +0700

    add initial code for stable diffusion

---
+++ stable_diffusion/sd/encoder.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn 
+from torch.nn import functional as F
+from decoder import VAE_AttentionBlock, VAE_ResidualBlock
+
+class VAE_Encoder(nn.Sequential):
+    def __init__(self):
+        super.__init__(
+            # Input: [B, 3, H, W]
+            nn.Conv2d(3, 128, kernel_size=3,padding=1), # [B, 128, H, W]
+            VAE_ResidualBlock(128, 128), # [B, 128, H, W]
+            VAE_ResidualBlock(128, 128), # [B, 128, H, W]
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0), # [B, 128, H/2, W/2]
+            VAE_ResidualBlock(128, 256), # [B, 128, H/2, W/2]
+            VAE_ResidualBlock(256, 256), # [B, 128, H/2, W/2]
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0), # [B, 256, H/4, W/4]
+            VAE_ResidualBlock(256,512),  # [B, 512, H/4, W/4]
+            VAE_ResidualBlock(512,512),  # [B, 512, H/4, W/4]
+            nn.Conv2d(512,512, kernel_size=3, stride=2, padding=0), # [B, 512, H/8, W/8]
+            VAE_ResidualBlock(512,512),  # [B, 512, H/8, W/8]
+            VAE_ResidualBlock(512,512),  # [B, 512, H/8, W/8]
+            VAE_ResidualBlock(512,512),  # [B, 512, H/8, W/8]
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512,512),  # [B, 512, H/8, W/8]
+            nn.GroupNorm(32,512),
+            nn.SiLU(),  # [B, 512, H/8, W/8]
+            nn.Conv2d(512, 8, kernel_size=3, padding=1), # [B, 8, H/8, W/8]
+            nn.Conv2d(8, 8, kernel_size=1, padding=0), # [B, 8, H/8, W/8]
+        )
+    def forward(self, x: torch.Tensor, noise: torch.Tensor) -> torch.Tensor:
+        '''
+        Input: x: [B, C, H, W], noise: (B, 8, H/8, W/8)
+
+
+        '''
+        for module in self:
+            if getattr(module, "stride", None) == (2,2):
+                x = F.pad(x, (0, 1, 0, 1)) # Add top and bottom
+            x = module(x) 
+        
+        mean, log_variance = torch.chunk(x, 2, dim=1) # 2 tensor([B, 4, H/8, W/8])
+        log_variance = torch.clamp(log_variance, -30, 20) # [B, 4, H/8, W/8]
+        variance = log_variance.exp() # [B, 4, H/8, W/8]
+        stdev = variance.sqrt() # [B, 4, H/8, W/8] 
+        x = mean + stdev * noise
+        x *= 0.18215
+        return x
